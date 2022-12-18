8660c7e4b2d173e52d89333e98a989989462e8ce
DevExpressExampleBot <fakeEmail>
2022-12-18 19:41:46 +0400

    Source auto update [skip ci]

--- VB/SqlGeometryDataAdapterExample/MainWindow.xaml.vb
+++ VB/SqlGeometryDataAdapterExample/MainWindow.xaml.vb
@@ -1,42 +1,43 @@
-﻿Imports DevExpress.Xpf.Map
+Imports DevExpress.Xpf.Map
 Imports System
 Imports System.Data.SqlClient
 Imports System.IO
 Imports System.Windows
 
 Namespace SqlGeometryDataAdapterExample
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Private Const filePath As String = "..\..\Data\SQLG.mdf"
-		Private Shared fullFilePath As String = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filePath))
-		Public ReadOnly Property ConnectionString() As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=" & fullFilePath & ";Database=SqlGeometryDB;Integrated Security=True;MultipleActiveResultSets=True"
+    Public Partial Class MainWindow
+        Inherits System.Windows.Window
 
-		Public Sub New()
-			InitializeComponent()
-			Me.DataContext = ConnectionString
-		End Sub
+        Const filePath As String = "..\..\Data\SQLG.mdf"
 
-		Private Sub MapEditor_MapItemEdited(ByVal sender As Object, ByVal e As DevExpress.Xpf.Map.MapItemEditedEventArgs)
-			For Each path As MapPath In e.Items
-				Dim id As Integer = Convert.ToInt32(path.Attributes("id").Value)
-				Dim modified As String = path.ExportToWkt().ToString()
-				path.Attributes("TextCol").Value = "Australia" & " " & DateTime.Now.Second.ToString()
+        Private Shared fullFilePath As String = System.IO.Path.GetFullPath(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, SqlGeometryDataAdapterExample.MainWindow.filePath))
 
-				Using cn As New SqlConnection() With {.ConnectionString = Me.ConnectionString}
-					cn.Open()
-					' For more information about SRID parameters, see https://docs.microsoft.com/en-us/sql/t-sql/spatial-geography/stsrid-geography-data-type?view=sql-server-ver15
-					Dim updatecmd As New SqlCommand("UPDATE DemoTable SET GeomCol1 = geometry::STGeomFromText('" & modified & "', 4326 ) WHERE id = " & id.ToString(), cn)
-					updatecmd.ExecuteNonQuery()
+        Public ReadOnly Property ConnectionString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=" & SqlGeometryDataAdapterExample.MainWindow.fullFilePath & ";Database=SqlGeometryDB;Integrated Security=True;MultipleActiveResultSets=True"
 
-					Dim updateattr As New SqlCommand("UPDATE DemoTable SET TextCol = '" & path.Attributes("TextCol").Value.ToString() & "' WHERE id =" & id.ToString(), cn)
-					updateattr.ExecuteNonQuery()
-				End Using
-			Next path
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.DataContext = Me.ConnectionString
+        End Sub
 
-		Private Sub VectorLayer_DataLoaded(ByVal sender As Object, ByVal e As DataLoadedEventArgs)
-			mapControl.ZoomToFitLayerItems()
-		End Sub
-	End Class
+        Private Sub MapEditor_MapItemEdited(ByVal sender As Object, ByVal e As DevExpress.Xpf.Map.MapItemEditedEventArgs)
+            For Each path As DevExpress.Xpf.Map.MapPath In e.Items
+                Dim id As Integer = System.Convert.ToInt32(path.Attributes(CStr(("id"))).Value)
+                Dim modified As String = path.ExportToWkt().ToString()
+                path.Attributes(CStr(("TextCol"))).Value = "Australia" & " " & System.DateTime.Now.Second.ToString()
+                Using cn As System.Data.SqlClient.SqlConnection = New System.Data.SqlClient.SqlConnection() With {.ConnectionString = Me.ConnectionString}
+                    cn.Open()
+                    ' For more information about SRID parameters, see https://docs.microsoft.com/en-us/sql/t-sql/spatial-geography/stsrid-geography-data-type?view=sql-server-ver15
+                    Dim updatecmd As System.Data.SqlClient.SqlCommand = New System.Data.SqlClient.SqlCommand("UPDATE DemoTable SET GeomCol1 = geometry::STGeomFromText('" & modified & "', 4326 ) WHERE id = " & id.ToString(), cn)
+                    updatecmd.ExecuteNonQuery()
+                    Dim updateattr As System.Data.SqlClient.SqlCommand = New System.Data.SqlClient.SqlCommand("UPDATE DemoTable SET TextCol = '" & path.Attributes(CStr(("TextCol"))).Value.ToString() & "' WHERE id =" & id.ToString(), cn)
+                    updateattr.ExecuteNonQuery()
+                End Using
+            Next
+        End Sub
+
+        Private Sub VectorLayer_DataLoaded(ByVal sender As Object, ByVal e As DevExpress.Xpf.Map.DataLoadedEventArgs)
+            Me.mapControl.ZoomToFitLayerItems()
+        End Sub
+    End Class
 End Namespace
